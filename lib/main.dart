6fd341c7a0a08cdf5650aa8be1a8ed8583766f31
arthurmgui <arthurmottaguimaraes@outlook.com>
2022-06-29 06:16:59 -0300

    acrescentei lógica no aplicativo após fazer a interface do app. Adicionei font para os textos.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +12,41 @@ class homePage extends StatefulWidget {
 }
 
 class _homePageState extends State<homePage> {
+  //Criando Array com frases
+  var listFrases = [
+    "O arrependimento diário é o caminho para a pureza.",
+    " a pureza traz poder. A pureza pessoal pode fazer de nós ferramentas poderosas nas mãos de Deus.",
+    "Jesus sofreu profundamente porque nos ama profundamente!",
+    "Ele quer que nos arrependamos e nos convertamos, para poder curar-nos mais plenamente."
+  ];
+
+  // Armazenando caminho das imagens
+  var imgBiscoito = "images/biscoito_inteiro.png";
+
+  //Frase da sorte
+  var fraseSorte = "Clique no botão para abrir o biscoito";
+
+  //botão de quebrar o biscoito
+  var btnBiscoito = "Abrir biscoito";
+
+//Método que quebra o biscoito
+  void quebrarBiscoito() {
+    var numero = Random().nextInt(listFrases.length);
+    if (btnBiscoito == "Abrir biscoito") {
+      setState(() {
+        imgBiscoito = "images/biscoito_quebrado.png";
+        fraseSorte = listFrases[numero];
+        btnBiscoito = "pegar outro biscoito";
+      });
+    } else {
+      setState(() {
+        imgBiscoito = "images/biscoito_inteiro.png";
+        fraseSorte = "Clique no botão para abrir o biscoito";
+        btnBiscoito = "Abrir biscoito";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,18 +62,18 @@ class _homePageState extends State<homePage> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               CircleAvatar(
-                backgroundImage: AssetImage("images/biscoito_inteiro.png"),
+                backgroundImage: AssetImage(imgBiscoito),
                 radius: 80,
                 backgroundColor: Colors.white,
               ),
               Text(
-                "Clique no botão para abrir o biscoito",
+                fraseSorte,
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 20, fontFamily: 'Pacifico'),
               ),
               ElevatedButton(
-                  onPressed: () {},
-                  child: Text("Abrir biscoito"),
+                  onPressed: quebrarBiscoito,
+                  child: Text(btnBiscoito),
                   style: ElevatedButton.styleFrom(
                       primary: Colors.amber, onPrimary: Colors.white))
             ],
